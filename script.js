4a9221a429cf193e8522bee0e43cfac0f6150744
jhurtadosandoval <120607545+jhurtadosandoval@users.noreply.github.com>
2023-08-13 16:33:08 -0700

    commit

--- script.js
+++ script.js
@@ -1,36 +1,75 @@
-const userMessage = [
-  "Can you share a screenshot?"
-  ];
-  const botReply = [
-  "hi"
-  ];
-  
-  const alternative = [
-    "not working"
-  ];
-  
-  const synth = window.speechSynthesis;
-  
-  // function voiceControl(string) {
-  //   let u = new SpeechSynthesisUtterance(string);
-  //   u.text = string;
-  //   u.lang = "en-aus";
-  //   u.volume = 1;
-  //   u.rate = 1;
-  //   u.pitch = 1;
-  //   synth.speak(u);
-  // }
-  
-  function sendMessage(choice) {
-    // const inputField = document.getElementById("input");
-    // let input = inputField.value.trim();
-    // input != "" && output(input);
-    // inputField.value = "";
-
-    output(choice);
+var stateVar = ""
 
 
+
+// const button = document.querySelector("hello")
+// const choicer = button.getAttribute("data-choice")
+  
+
+
+function sendMessage(choice) {
+
+
+  output(choice);
+  product = choice
+
+}
+
+  function showDefaultChoices(){
+    const choiceButtons = document.getElementById("choiceButtons");
+    const defaultChoices = [
+      "Check user permissions",
+      "Check the API Response",
+      "Correct Usage of Endpoint"
+    ];
+
+    defaultChoices.forEach((choice) => {
+      const button = document.createElement("button");
+      button.textContent = choice;
+      button.onclick = () => sendMessage(choice);
+      choiceButtons.appendChild(button);
+    });
   }
+
+  function showResponseChoices(){
+    const choiceButtons = document.getElementById("choiceButtons");
+    const defaultChoices = [
+      "Are you getting an error code?",
+      "What is the full response you get from the API?",
+      "Back"
+    ];
+
+    
+
+    defaultChoices.forEach((choice) => {
+      const button = document.createElement("button");
+      button.textContent = choice;
+      button.onclick = () => sendMessage(choice);
+      choiceButtons.appendChild(button);
+    });
+    
+  }
+
+  function showPermissionChoices(){
+    const choiceButtons = document.getElementById("choiceButtons");
+    const defaultChoices = [
+      "Do you own the Project?",
+      "What are the scopes of your Token?",
+      "Back"
+    ];
+
+    
+
+    defaultChoices.forEach((choice) => {
+      const button = document.createElement("button");
+      button.textContent = choice;
+      button.onclick = () => sendMessage(choice);
+      choiceButtons.appendChild(button);
+    });
+    
+  }
+
+
   document.addEventListener("DOMContentLoaded", () => {
     const inputField = document.getElementById("input");
     inputField.addEventListener("keydown", function (e) {
@@ -43,90 +82,84 @@ const userMessage = [
   });
   
   function output(input) {
-    let product;
-  
-    let text = input.toLowerCase().replace(/[^\w\s\d]/gi, "");
-  
-    text = text
-      .replace(/[\W_]/g, " ")
-      .replace(/ a /g, " ")
-      .replace(/i feel /g, "")
-      .replace(/whats/g, "what is")
-      .replace(/please /g, "")
-      .replace(/ please/g, "")
-      .trim();
-  
-    // let comparedText = compare(userMessage, botReply, text);
-  
-    // product = compare(userMessage,botReply,text);
-    // if (text.includes("Can you share a screenshot?")){
-    //   product ="hee"
-    //   console.log(text)
-    // }
 
-    // else if (userMessage.includes("Are you getting an error code?")){
-    //   product = alternative[Math.floor(Math.random() * alternative.length)]
-    //   product="Hi"
-    // }
   
-    product = "?"
-    console.log(text)
-    if (text.includes("are you getting an error code")){
-      product = "No, the call is successfull. "
+    let text = input.toLowerCase();
+  
+    stateVar = text;
+    console.log(stateVar);
+
+    if (stateVar ==="check user permissions"){
+     choiceButtons.innerHTML = "";
+     showPermissionChoices();
 
     }
-    else if (text.includes("can you share screenshot")){
-      product="Sure thing.";
+    else if (text ===("check the api response")){
+      choiceButtons.innerHTML = "";
+      showResponseChoices();
+
+
     }
-    else if (text.includes("tell me more about your token"))
-    {
-      product = "What do you want to know?"
+    
+    else if (stateVar === "correct usage of endpoint"){
+      choiceButtons.innerHTML = "";
+      showResponseChoices();
+
+
+    }
+    else if (stateVar === "are you getting an error code?"){
+      var question = "Are you getting an error code in your response fromt the API?";
+      var answer = "No, I am getting a 200 status, so the call is successful.";
+      addChat(question,answer)
 
     }
 
+    else if (stateVar === "what is the full response you get from the api?"){
+      var question = "Could you share with us the full response you are getting from your call?";
+      var answer = "Sure. ";
+      addChat(question,answer)
 
-    addChat(input, product);
+    }
+
+    else if (stateVar==="back"){
+      choiceButtons.innerHTML = "";
+      showDefaultChoices();
+    }
+
+    else if (stateVar === "do you own the project?"){
+      var question = "Are you the owner of the account and project you are trying to create a new asset in?";
+      var answer = "Yes, I am the account owner and also own the project. I created my developer Token using my owner account.";
+      addChat(question,answer)
+
+    }
+
+    else if (stateVar === "what are the scopes of your token?"){
+      var question = "What are the permissions you enabled for your token when you created it?";
+      var answer = "I selected all scopes for my token. Also, I don't think it's a permission issue since a new queued asset is created and I get a 200 status response that my call to create the asset was successful.";
+      addChat(question,answer)
+
+    }
+
+    
+
+    
+
+
+  //line used to add selected choice to chatbox
+    //addChat(input, product);
   }
   
-  function compare(triggerArray, replyArray, string) {
-    let item;
-    for (let x = 0; x < triggerArray.length; x++) {
-        if (triggerArray[x].includes(string)) {
-          items = replyArray[x];
-          item = items[Math.floor(Math.random() * items.length)];
-        }
-    }
-    return item;
-  }
-  
-  function containMessageCheck(string) {
-    let expectedReply = [
-      [
-        "Tell me more about your token.",
-        "Bye, See you!",
-        "Dude, Bye. Take care of your health in this situation."
-      ],
-      ["Tell me more about your token.", "Have a sound sleep", "Sweet dreams"],
-      ["Have a pleasant evening!", "Good evening too", "Evening!"],
-      ["Good morning, Have a great day!", "Morning, dude!"],
-      ["Good Afternoon", "Noon, dude!", "Afternoon, dude!"]
-    ];
-    let expectedMessage = [
-      ["Tell me more about your token.", "tc", "take care"],
-      ["night", "good night"],
-      ["evening", "good evening"],
-      ["morning", "good morning"],
-      ["noon"]
-    ];
-    let item;
-    for (let x = 0; x < expectedMessage.length; x++) {
-      if (expectedMessage[x].includes(string)) {
-        items = expectedReply[x];
-        item = items[Math.floor(Math.random() * items.length)];
-      }
-    }
-    return item;
-  }
+  // function compare(triggerArray, replyArray, string) {
+  //   let item;
+  //   for (let x = 0; x < triggerArray.length; x++) {
+  //       if (triggerArray[x].includes(string)) {
+  //         items = replyArray[x];
+  //         item = items[Math.floor(Math.random() * items.length)];
+  //       }
+  //   }
+  //   return item;
+  // }
+
   function addChat(input, product) {
     const mainDiv = document.getElementById("message-section");
     let userDiv = document.createElement("div");
@@ -142,5 +175,5 @@ const userMessage = [
     mainDiv.appendChild(botDiv);
     var scroll = document.getElementById("message-section");
     scroll.scrollTop = scroll.scrollHeight;
-    // voiceControl(product);
+
   }
